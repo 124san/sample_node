fe862f6e4356b312b28588faeb3399bc9b8b8973
Dehou Kong <dehou.kong@mail.utoronto.ca>
2019-09-17 16:19:21 -0400

    webpage back baby

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 80
-const webPath = 'var/www/www.124san.com/index.html'
 
-app.get('/', (req, res) => res.sendFile(webPath))
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
 app.listen(port, () => console.log(`My app listening on port ${port}!`))
+app.listen(port, () => console.log(`My app listening on dirname ${__dirname}!`))
