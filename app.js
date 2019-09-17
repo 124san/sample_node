c39c5ee4363778378f3f7811c0de9c6d6aea822b
Dehou Kong <dehou.kong@mail.utoronto.ca>
2019-09-17 16:15:52 -0400

    change app name and added web pages

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 80
+const webPath = './var/www/www.124san.com/index.html'
 
-app.get('/', (req, res) => res.sendFile("index.html"))
+app.get('/', (req, res) => res.sendFile(webPath))
 
 app.listen(port, () => console.log(`My app listening on port ${port}!`))
